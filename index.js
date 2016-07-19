b6d99d03f4165aa5532c94377c846c76adfe1a09
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-07-19 08:23:19 -0300

    Added social icons and changed layout to get rid of header menu

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function slideIn (el) {
+function headerSlider (el) {
+  var hide = true;
   var elem = document.getElementById(el);
   console.log(el);
+  elem.style.top = 0;
 //  elem.style.transition = "top 1.5s linear 0s";
 //  elem.style.top = "0";
 }
 
-function slideOut (el) {
-    var elem = document.getElementById(el);
-    elem.style.transition = "left 1.5s linear 0s";
-    elem.style.left = "-200px";
+function headerSlideOut (el) {
+  var elem = document.getElementById(el);
+  elem.style.transition = "left 1.5s linear 0s";
+  elem.style.left = "-200px";
 }
