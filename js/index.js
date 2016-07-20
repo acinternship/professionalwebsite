981e950cb1e846a20e4cbb08e90f9a91c1d71a7b
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-20 15:41:11 -0300

    Added bxSlider jQuery lib

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // Show/hide content according to button click
+  $('#header-work').click(function () {
+    $('#content-work').slideToggle('slow');
+    $('#header-work').children('i').toggleClass('rotate-180deg');
+  });
+  
+  $('#header-design').click(function () {
+    $('#content-design').slideToggle('slow');
+    $('#header-design').children('i').toggleClass('rotate-180deg');
+  });
+  
+  $('#header-drawings').click(function () {
+    $('#content-drawings').slideToggle('slow');
+    $('#header-drawings').children('i').toggleClass('rotate-180deg');
+  });
+  
+  // Slide content
+  $('#slider-design')
+});
