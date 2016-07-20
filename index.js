e94c47a9f65c2883274daa04b227331c0e0fa614
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-20 10:46:38 -0300

    Added gitignore file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 
 $(document).ready(function () {
   $('header-work').click(function () {
+    console.log("Hey!");
     $('content-work').slideToggle('slow');
   });
 });
