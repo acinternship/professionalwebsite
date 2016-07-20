9d983f94f2b5c0f2ec86150b0814d19408817b6e
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-20 10:37:11 -0300

    Expanded header height and changed experience div layout

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function headerSlider (el) {
-  var hide = true;
-  var elem = document.getElementById(el);
-  console.log(el);
-  elem.style.top = 0;
-//  elem.style.transition = "top 1.5s linear 0s";
-//  elem.style.top = "0";
-}
+var $ = require('jquery');
 
-function headerSlideOut (el) {
-  var elem = document.getElementById(el);
-  elem.style.transition = "left 1.5s linear 0s";
-  elem.style.left = "-200px";
-}
+$(document).ready(function () {
+  $('header-work').click(function () {
+    $('content-work').slideToggle('slow');
+  });
+});
