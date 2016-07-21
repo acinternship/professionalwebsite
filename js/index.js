f994b7e5f854fabcf0696168ae89e66f785935b7
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-07-20 23:01:36 -0300

    Added logo slideshow and box-shadow to some elements

--- js/index.js
+++ js/index.js
@@ -14,12 +14,28 @@ $(document).ready(function () {
     $('#content-drawings').slideToggle('slow');
     $('#header-drawings').children('i').toggleClass('rotate-180deg');
   });
-  
-  // Slide content
-  $('#slider-design').bxSlider({
-    slideWidth: 300,
-    minSlides: 1,
-    maxSlides: 3,
-    slideMargin: 10
-  })
 });
+
+// Slider
+var slideIndex = 1;
+
+function slideShow(n) {
+  var slideNum = $('#slider-design div').length - 1;
+  
+  if (n > slideNum)
+    slideIndex = 1;
+
+  if (n < 1)
+    slideIndex = slideNum;
+
+  for (var i = 0; i < slideNum; i++)
+    $('#slider-design div')[i].style.display = 'none';
+  
+  $('#slider-design div')[slideIndex-1].style.display = 'flex';
+}
+
+slideShow(slideIndex);
+
+function slideSwitch(i) {
+  slideShow(slideIndex += i);
+}
